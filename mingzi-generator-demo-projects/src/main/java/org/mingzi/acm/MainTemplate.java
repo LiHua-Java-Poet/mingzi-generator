518819e7be7de37405b28314722c12c749ce6c2c
LiHua-Java-Poet <1550217434@qq.com>
2024-12-01 15:20:18 +0800

    初始化项目结构

---
+++ mingzi-generator-demo-projects/src/main/java/org/mingzi/acm/MainTemplate.java
@@ -0,0 +1,31 @@
+package org.mingzi.acm;
+
+import java.util.Scanner;
+
+public class MainTemplate {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+
+            int n = scanner.nextInt();
+
+            int[] arr = new int[n];
+
+            for (int i = 0; i < args.length; i++) {
+                arr[i] = scanner.nextInt();
+            }
+
+
+            int sum = 0;
+            for (int num : arr) {
+                sum += sum;
+            }
+
+            System.out.println("Sum:" + sum);
+
+        }
+        scanner.close();
+    }
+}
