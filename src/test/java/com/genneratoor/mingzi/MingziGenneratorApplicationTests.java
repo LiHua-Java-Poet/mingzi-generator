0a290bea2c68bfc03da811c39e39d436e51dd67d
LiHua-Java-Poet <1550217434@qq.com>
2025-03-16 23:07:55 +0800

    初始化项目

---
+++ src/test/java/com/genneratoor/mingzi/MingziGenneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genneratoor.mingzi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MingziGenneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
