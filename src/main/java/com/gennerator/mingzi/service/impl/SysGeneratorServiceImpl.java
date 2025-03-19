8365b9b2419e1c29f46da31134d817ffacf58703
LiHua-Java-Poet <1550217434@qq.com>
2025-03-19 21:35:48 +0800

    完成TO,VO的代码生成

--- src/main/java/com/gennerator/mingzi/service/impl/SysGeneratorServiceImpl.java
+++ src/main/java/com/gennerator/mingzi/service/impl/SysGeneratorServiceImpl.java
@@ -46,16 +46,15 @@ public class SysGeneratorServiceImpl implements SysGeneratorService {
             List<Map<String, Object>> columnList = mySQLGeneratorDao.selectTableInfo(tableName.toString());
             tableEntity.setTableName(tableName.toString());
             tableEntity.setComments(item.get("Comment").toString());
-            tableEntity.setClassname(toLowerCamelCase(tableEntity.getTableName()));
-            tableEntity.setClassName(toUpperCamelCase(tableEntity.getTableName()));
+            tableEntity.setClassname(GenUtils.toLowerCamelCase(tableEntity.getTableName()));
+            tableEntity.setClassName(GenUtils.toUpperCamelCase(tableEntity.getTableName()));
 
             Map<String, Object> primaryKeyMap = columnList.stream().filter(b -> "PRI".equals(b.get("columnKey"))).findFirst().orElse(null);
-            ColumnEntity columnEntity = mapToColumnEntity(primaryKeyMap);
+            ColumnEntity columnEntity = GenUtils.mapToColumnEntity(primaryKeyMap);
             tableEntity.setPk(columnEntity);
-            columnList.remove(primaryKeyMap);
 
             List<ColumnEntity> columnEntities = columnList.stream().map(columnItem -> {
-                return mapToColumnEntity(columnItem);
+                return GenUtils.mapToColumnEntity(columnItem);
             }).collect(Collectors.toList());
             tableEntity.setColumns(columnEntities);
 
@@ -74,147 +73,4 @@ public class SysGeneratorServiceImpl implements SysGeneratorService {
         return outputStream.toByteArray();
     }
 
-    /**
-     * 将 primaryKeyMap 封装为 ColumnEntity 对象
-     * @param primaryKeyMap 表属性的 Map
-     * @return 封装好的 ColumnEntity 对象
-     */
-    public static ColumnEntity mapToColumnEntity(Map<String, Object> primaryKeyMap) {
-        ColumnEntity columnEntity = new ColumnEntity();
-
-        // 设置列名
-        if (primaryKeyMap.containsKey("columnName")) {
-            columnEntity.setColumnName((String) primaryKeyMap.get("columnName"));
-        }
-
-        // 设置列名类型
-        if (primaryKeyMap.containsKey("dataType")) {
-            columnEntity.setDataType((String) primaryKeyMap.get("dataType"));
-        }
-
-        // 设置列名备注
-        if (primaryKeyMap.containsKey("columnComment")) {
-            columnEntity.setComments((String) primaryKeyMap.get("columnComment"));
-        }
-
-        // 设置属性名称（第一个字母大写）
-        if (primaryKeyMap.containsKey("columnName")) {
-            String columnName = (String) primaryKeyMap.get("columnName");
-            String attrName = toUpperCamelCase(columnName); // 转换为大驼峰
-            columnEntity.setAttrName(attrName);
-        }
-
-        // 设置属性名称（第一个字母小写）
-        if (primaryKeyMap.containsKey("columnName")) {
-            String columnName = (String) primaryKeyMap.get("columnName");
-            String attrname = toLowerCamelCase(columnName); // 转换为小驼峰
-            columnEntity.setAttrNameSmall(attrname);
-        }
-
-        // 设置属性类型
-        if (primaryKeyMap.containsKey("dataType")) {
-            String dataType = (String) primaryKeyMap.get("dataType");
-            String attrType = convertDataTypeToJavaType(dataType); // 转换为 Java 类型
-            columnEntity.setAttrType(attrType);
-        }
-
-        // 设置 auto_increment
-        if (primaryKeyMap.containsKey("extra")) {
-            columnEntity.setExtra((String) primaryKeyMap.get("extra"));
-        }
-
-        return columnEntity;
-    }
-
-    /**
-     * 将数据库字段类型转换为 Java 类型
-     * @param dataType 数据库字段类型
-     * @return Java 类型
-     */
-    private static String convertDataTypeToJavaType(String dataType) {
-        if (dataType == null || dataType.isEmpty()) {
-            return "String"; // 默认返回 String
-        }
-        switch (dataType.toLowerCase()) {
-            case "int":
-            case "integer":
-                return "Integer";
-            case "bigint":
-                return "Long";
-            case "varchar":
-            case "text":
-            case "char":
-                return "String";
-            case "datetime":
-            case "timestamp":
-                return "Date";
-            case "decimal":
-            case "numeric":
-                return "BigDecimal";
-            case "boolean":
-            case "tinyint":
-                return "Boolean";
-            case "float":
-                return "Float";
-            case "double":
-                return "Double";
-            default:
-                return "String"; // 默认返回 String
-        }
-    }
-
-    /**
-     * 将表名转换为大驼峰格式
-     *
-     * @param tableName 表名（如：user_info）
-     * @return 大驼峰格式的字符串（如：UserInfo）
-     */
-    public static String toUpperCamelCase(String tableName) {
-        if (tableName == null || tableName.isEmpty()) {
-            return tableName;
-        }
-
-        // 将表名按分隔符（如下划线）拆分为单词数组
-        String[] words = tableName.split("_");
-        StringBuilder result = new StringBuilder();
-
-        // 每个单词首字母大写
-        for (String word : words) {
-            if (!word.isEmpty()) {
-                result.append(word.substring(0, 1).toUpperCase())
-                        .append(word.substring(1).toLowerCase());
-            }
-        }
-
-        return result.toString();
-    }
-
-    /**
-     * 将表名转换为小驼峰格式
-     *
-     * @param tableName 表名（如：user_info）
-     * @return 小驼峰格式的字符串（如：userInfo）
-     */
-    public static String toLowerCamelCase(String tableName) {
-        if (tableName == null || tableName.isEmpty()) {
-            return tableName;
-        }
-
-        // 将表名按分隔符（如下划线）拆分为单词数组
-        String[] words = tableName.split("_");
-        StringBuilder result = new StringBuilder();
-
-        // 第一个单词全小写
-        result.append(words[0].toLowerCase());
-
-        // 后续单词首字母大写
-        for (int i = 1; i < words.length; i++) {
-            if (!words[i].isEmpty()) {
-                result.append(words[i].substring(0, 1).toUpperCase())
-                        .append(words[i].substring(1).toLowerCase());
-            }
-        }
-
-        return result.toString();
-    }
 }
--- src/main/java/com/gennerator/mingzi/utils/GenUtils.java
+++ src/main/java/com/gennerator/mingzi/utils/GenUtils.java
@@ -2,8 +2,10 @@ package com.gennerator.mingzi.utils;
 
 //import cn.hutool.core.io.IoUtil;
 //import cn.hutool.core.util.StrUtil;
+
 import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.util.StrUtil;
+import com.gennerator.mingzi.entity.ColumnEntity;
 import org.apache.commons.configuration.Configuration;
 //import org.apache.commons.configuration.ConfigurationException;
 //import org.apache.commons.configuration.PropertiesConfiguration;
@@ -13,7 +15,6 @@ import org.apache.commons.configuration.Configuration;
 //import org.apache.velocity.VelocityContext;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.Velocity;
 
 import com.gennerator.mingzi.entity.TableEntity;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -21,7 +22,6 @@ import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
-import javax.naming.ConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
@@ -33,7 +33,11 @@ public class GenUtils {
 
     public static List<String> getTemplates() {
         List<String> templates = new ArrayList<>();
-        templates.add("template/Entity.java.vm" );
+        templates.add("template/Entity.java.vm");
+        templates.add("template/InfoTo.java.vm");
+        templates.add("template/ListTo.java.vm");
+        templates.add("template/SaveVo.java.vm");
+        templates.add("template/UpdateVo.java.vm");
         return templates;
     }
 
@@ -43,18 +47,17 @@ public class GenUtils {
     public static void generatorCode(TableEntity tableEntity, ZipOutputStream zip) {
         Configuration config = getConfig();
 
-        Iterator<String> keys = config.getKeys();
         Map<String, Object> map = new HashMap<>();
-        map.put("tableName" , tableEntity.getTableName());
-        map.put("comments" , tableEntity.getComments());
-        map.put("pk" , tableEntity.getPk());
-        map.put("className" , tableEntity.getClassName());
-        map.put("classname" , tableEntity.getClassname());
-        map.put("pathName" , tableEntity.getClassname().toLowerCase());
-        map.put("columns" , tableEntity.getColumns());
-        map.put("package" , config.getString("package" ));
-        map.put("moduleName" , config.getString("moduleName" ));
-        map.put("author" , config.getString("author" ));
+        map.put("tableName", tableEntity.getTableName());
+        map.put("comments", tableEntity.getComments());
+        map.put("pk", tableEntity.getPk());
+        map.put("className", tableEntity.getClassName());
+        map.put("classname", tableEntity.getClassname());
+        map.put("pathName", tableEntity.getClassname().toLowerCase());
+        map.put("columns", tableEntity.getColumns());
+        map.put("package", config.getString("package"));
+        map.put("moduleName", config.getString("moduleName"));
+        map.put("author", config.getString("author"));
 
         VelocityContext context = new VelocityContext(map);
 
@@ -73,14 +76,13 @@ public class GenUtils {
             // 2. 初始化引擎（必须调用 init()）
             velocityEngine.init();
 
-            // 3. 加载模板（路径相对于 resources 根目录）
-            String templatePath = "template/Entity.java.vm"; // 不要以斜杠开头
-            Template tpl = velocityEngine.getTemplate(templatePath, "UTF-8");
+            Template tpl = velocityEngine.getTemplate(template, "UTF-8");
             tpl.merge(context, sw);
 
             try {
+                String fileName = getFileName(template, tableEntity.getClassName(), config.getString("package"), config.getString("moduleName"), tableEntity);
                 //添加到zip
-                zip.putNextEntry(new ZipEntry(getFileName(template, tableEntity.getClassName(), config.getString("package" ), config.getString("moduleName" ))));
+                zip.putNextEntry(new ZipEntry(fileName));
                 IoUtil.writeUtf8(zip, false, sw.toString());
                 zip.flush();
                 IoUtil.close(sw);
@@ -97,77 +99,232 @@ public class GenUtils {
      */
     public static Configuration getConfig() {
         try {
-            return new PropertiesConfiguration("generator.properties" );
+            return new PropertiesConfiguration("generator.properties");
         } catch (Exception e) {
-            throw new RuntimeException("获取配置文件失败，" , e);
+            throw new RuntimeException("获取配置文件失败，", e);
         }
     }
 
     /**
      * 获取文件名
      */
-    public static String getFileName(String template, String className, String packageName, String moduleName) {
+    public static String getFileName(String template, String className, String packageName, String moduleName, TableEntity tableEntity) {
         String packagePath = "main" + File.separator + "java" + File.separator;
         if (StrUtil.isNotBlank(packageName)) {
-            packagePath += packageName.replace("." , File.separator) + File.separator + "modules" + File.separator + moduleName + File.separator;
+            packagePath += packageName.replace(".", File.separator) + File.separator;
         }
 
-        if (template.contains("Entity.java.vm" )) {
-            return packagePath + "entity" + File.separator + className + "Entity.java";
+        if (template.contains("Entity.java.vm")) {
+            return packagePath + "model" + File.separator + "entity" + File.separator + className + "Entity.java";
         }
 
-        if (template.contains("Excel.java.vm" )) {
-            return packagePath + "excel" + File.separator + className + "Excel.java";
-        }
-
-        if (template.contains("Dao.java.vm" )) {
+        if (template.contains("Dao.java.vm")) {
             return packagePath + "dao" + File.separator + className + "Dao.java";
         }
 
-        if (template.contains("Service.java.vm" )) {
+        if (template.contains("Service.java.vm")) {
             return packagePath + "service" + File.separator + className + "Service.java";
         }
 
-        if (template.contains("ServiceImpl.java.vm" )) {
+        if (template.contains("ServiceImpl.java.vm")) {
             return packagePath + "service" + File.separator + "impl" + File.separator + className + "ServiceImpl.java";
         }
 
-        if (template.contains("Controller.java.vm" )) {
+        if (template.contains("Controller.java.vm")) {
             return packagePath + "controller" + File.separator + className + "Controller.java";
         }
 
-        if (template.contains("Dao.xml.vm" )) {
+        if (template.contains("Dao.xml.vm")) {
             return "main" + File.separator + "resources" + File.separator + "mapper" + File.separator + moduleName + File.separator + className + "Dao.xml";
         }
 
-        if (template.contains("DTO.java.vm" )) {
-            return packagePath + "dto" + File.separator + className + "DTO.java";
+        if (template.contains("InfoTo.java.vm")) {
+            return packagePath + "model" + File.separator + "to" + File.separator + tableEntity.getClassname() + File.separator + className + "InfoTo.java";
         }
 
-        if (template.contains("index.vue.vm" )) {
+        if (template.contains("ListTo.java.vm")) {
+            return packagePath + "model" + File.separator + "to" + File.separator + tableEntity.getClassname() + File.separator + className + "ListTo.java";
+        }
+
+        if (template.contains("SaveVo.java.vm")) {
+            return packagePath + "model" + File.separator + "vo" + File.separator + tableEntity.getClassname() + File.separator + className + "SaveVo.java";
+        }
+
+        if (template.contains("UpdateVo.java.vm")) {
+            return packagePath + "model" + File.separator + "vo" + File.separator + tableEntity.getClassname() + File.separator + className + "UpdateVo.java";
+        }
+
+
+        if (template.contains("index.vue.vm")) {
             return "vue" + File.separator + "views" + File.separator + moduleName + File.separator + className.toLowerCase() + ".vue";
         }
 
-        if (template.contains("add-or-update.vue.vm" )) {
+        if (template.contains("add-or-update.vue.vm")) {
             return "vue" + File.separator + "views" + File.separator + moduleName + File.separator + className.toLowerCase() + "-add-or-update.vue";
         }
 
-        if (template.contains("mysql.vm" )) {
+        if (template.contains("mysql.vm")) {
             return className.toLowerCase() + ".mysql.sql";
         }
 
-        if (template.contains("oracle.vm" )) {
+        if (template.contains("oracle.vm")) {
             return className.toLowerCase() + ".oracle.sql";
         }
 
-        if (template.contains("sqlserver.vm" )) {
+        if (template.contains("sqlserver.vm")) {
             return className.toLowerCase() + ".sqlserver.sql";
         }
 
-        if (template.contains("postgresql.vm" )) {
+        if (template.contains("postgresql.vm")) {
             return className.toLowerCase() + ".postgresql.sql";
         }
 
         return null;
+    }
+
+    /**
+     * 将 primaryKeyMap 封装为 ColumnEntity 对象
+     *
+     * @param primaryKeyMap 表属性的 Map
+     * @return 封装好的 ColumnEntity 对象
+     */
+    public static ColumnEntity mapToColumnEntity(Map<String, Object> primaryKeyMap) {
+        ColumnEntity columnEntity = new ColumnEntity();
+
+        // 设置列名
+        if (primaryKeyMap.containsKey("columnName")) {
+            columnEntity.setColumnName((String) primaryKeyMap.get("columnName"));
+        }
+
+        // 设置列名类型
+        if (primaryKeyMap.containsKey("dataType")) {
+            columnEntity.setDataType((String) primaryKeyMap.get("dataType"));
+        }
+
+        // 设置列名备注
+        if (primaryKeyMap.containsKey("columnComment")) {
+            columnEntity.setComments((String) primaryKeyMap.get("columnComment"));
+        }
+
+        // 设置属性名称（第一个字母大写）
+        if (primaryKeyMap.containsKey("columnName")) {
+            String columnName = (String) primaryKeyMap.get("columnName");
+            String attrName = toUpperCamelCase(columnName); // 转换为大驼峰
+            columnEntity.setAttrName(attrName);
+        }
+
+        // 设置属性名称（第一个字母小写）
+        if (primaryKeyMap.containsKey("columnName")) {
+            String columnName = (String) primaryKeyMap.get("columnName");
+            String attrname = toLowerCamelCase(columnName); // 转换为小驼峰
+            columnEntity.setAttrNameSmall(attrname);
+        }
+
+        // 设置属性类型
+        if (primaryKeyMap.containsKey("dataType")) {
+            String dataType = (String) primaryKeyMap.get("dataType");
+            String attrType = convertDataTypeToJavaType(dataType); // 转换为 Java 类型
+            columnEntity.setAttrType(attrType);
+        }
+
+        // 设置 auto_increment
+        if (primaryKeyMap.containsKey("extra")) {
+            columnEntity.setExtra((String) primaryKeyMap.get("extra"));
+        }
+
+        return columnEntity;
+    }
+
+    /**
+     * 将数据库字段类型转换为 Java 类型
+     *
+     * @param dataType 数据库字段类型
+     * @return Java 类型
+     */
+    private static String convertDataTypeToJavaType(String dataType) {
+        if (dataType == null || dataType.isEmpty()) {
+            return "String"; // 默认返回 String
+        }
+        switch (dataType.toLowerCase()) {
+            case "int":
+            case "integer":
+            case "tinyint":
+                return "Integer";
+            case "bigint":
+                return "Long";
+            case "varchar":
+            case "text":
+            case "char":
+                return "String";
+            case "datetime":
+            case "timestamp":
+                return "Date";
+            case "decimal":
+            case "numeric":
+                return "BigDecimal";
+            case "boolean":
+                return "Boolean";
+            case "float":
+                return "Float";
+            case "double":
+                return "Double";
+            default:
+                return "String"; // 默认返回 String
+        }
+    }
+
+    /**
+     * 将表名转换为大驼峰格式
+     *
+     * @param tableName 表名（如：user_info）
+     * @return 大驼峰格式的字符串（如：UserInfo）
+     */
+    public static String toUpperCamelCase(String tableName) {
+        if (tableName == null || tableName.isEmpty()) {
+            return tableName;
+        }
+
+        // 将表名按分隔符（如下划线）拆分为单词数组
+        String[] words = tableName.split("_");
+        StringBuilder result = new StringBuilder();
+
+        // 每个单词首字母大写
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                result.append(word.substring(0, 1).toUpperCase())
+                        .append(word.substring(1).toLowerCase());
+            }
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * 将表名转换为小驼峰格式
+     *
+     * @param tableName 表名（如：user_info）
+     * @return 小驼峰格式的字符串（如：userInfo）
+     */
+    public static String toLowerCamelCase(String tableName) {
+        if (tableName == null || tableName.isEmpty()) {
+            return tableName;
+        }
+
+        // 将表名按分隔符（如下划线）拆分为单词数组
+        String[] words = tableName.split("_");
+        StringBuilder result = new StringBuilder();
+
+        // 第一个单词全小写
+        result.append(words[0].toLowerCase());
+
+        // 后续单词首字母大写
+        for (int i = 1; i < words.length; i++) {
+            if (!words[i].isEmpty()) {
+                result.append(words[i].substring(0, 1).toUpperCase())
+                        .append(words[i].substring(1).toLowerCase());
+            }
+        }
+
+        return result.toString();
     }
 }
